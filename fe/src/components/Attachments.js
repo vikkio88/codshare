b42da44e36ec064af7eddd78baa59e75ff99d75d
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-02-27 19:30:33 +0000

    started working on ui

--- fe/src/components/Attachments.js
+++ fe/src/components/Attachments.js
@@ -1,25 +1,38 @@
 import { MAX_ATTACHMENTS } from "../config";
 import Attachment from "./Attachment";
+import "./styles/Attachments.css";
 
 
 const Attachments = ({ weapon = null, attachments = [], onSet }) => {
     const selects = [...Array(Math.min(attachments.length + 1, MAX_ATTACHMENTS)).keys()];
-    return selects.map(i => {
-        return (
-            Boolean(weapon)
-            && (
-                <Attachment
-                    key={`attach_${i}`}
-                    number={i}
-                    /** NEED TO REMOVE THE OLD ATTACHMENT FIRST */
-                    onSelect={({ value: attachment }) => onSet({ attachments: [...attachments, attachment] })}
-                    weapon={weapon}
-                    attachments={attachments}
-                />
-            )
-        );
-
-    });
+    return <div className="attachmentList">
+        {selects.map(i => {
+            return (
+                Boolean(weapon)
+                && (
+                    <div key={`attach_${i}`}>
+                        <Attachment
+                            number={i}
+                            onSelect={({ value: attachment }) => {
+                                const newAttachments = [...attachments];
+                                newAttachments[i] = attachment;
+                                onSet({ attachments: newAttachments });
+                            }}
+                            weapon={weapon}
+                            attachments={attachments}
+                        />
+                        <button onClick={() => {
+                            // this needs to remove the specific attachment
+                            const newAttachments = [...attachments];
+                            newAttachments.splice(i, 1);
+                            onSet({ attachments: newAttachments });
+                        }}>x</button>
+                    </div>
+                )
+            );
+        })}
+    </div>;
 };
+
 
 export default Attachments;
