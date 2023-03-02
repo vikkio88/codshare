44fad16d625bb2bd48b6cf8ba04cfb7bb5a8e10e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-03-02 20:08:12 +0000

    added tuning info on view

--- fe/src/pages/View.js
+++ fe/src/pages/View.js
@@ -3,20 +3,29 @@ import { fromSlug } from '../libs/slugger';
 import AttachIndicators from '../components/AttachIndicators';
 import './styles/View.css';
 
+import map from "../data/db/map.json";
+
 const View = () => {
     const params = useParams();
     const to = useNavigate();
     const config = fromSlug(params.slug);
     const share = () => navigator.clipboard.writeText(`${window.location.origin}/view/${params.slug}`);
     return (
-        <>
+        <div className="View">
             <h1 className="weaponShow">{config.weapon.name}</h1>
+            <h2>({map.categories[config.weapon.category_id].name})</h2>
             <AttachIndicators attachmentsCount={config.attachments.length} />
             {config.attachments.map((a, i) => (
                 <div className="attachmentRow" key={a.id}>
                     <h2>
-                        {a.name}
+                        {a.name} ({map.attachmentCategories[a.category_id].name})
                     </h2>
+                    {config.tuning?.[i] && (
+                        <div>
+                            {Boolean(config.tuning?.[i][0]) && <h2 className="tuningVal">↕️ {config.tuning?.[i][0]}</h2>}
+                            {Boolean(config.tuning?.[i][1]) && <h2 className="tuningVal">↔️ {config.tuning?.[i][1]}</h2>}
+                        </div>
+                    )}
                 </div>
             ))}
             <div className="actionButtons">
@@ -24,7 +33,7 @@ const View = () => {
                 <button onClick={() => to(`/`)}>➕</button>
                 <button onClick={share}>🔗</button>
             </div>
-        </>
+        </div>
     );
 };
 
